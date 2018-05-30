18d8e0134ee339d7ebd141aed91e31d7a4265446
Tiffany Shanaz <32331601+Iayla@users.noreply.github.com>
2018-05-30 07:22:21 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 session_start();
 require_once "GoogleAPI/vendor/autoload.php";
 $gClient = new Google_Client();
-$gClient->setClientId();
-$gClient->setClientSecret();
+$gClient->setClientId(212347471103-p8l13e0fiejvf9ea2jt4igomp1a89l7m.apps.googleusercontent.com);
+$gClient->setClientSecret(T7oIGLTbgdfltaloxwx4QybD);
 $gClient-setApplicationName("Social Login");
 $gClient->setRedirectUri(http"//localhost:4433/OAuth-Social-Login/G-call back.php);
 $gClient->addScope(scope_or_scope:"https;//www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email")
